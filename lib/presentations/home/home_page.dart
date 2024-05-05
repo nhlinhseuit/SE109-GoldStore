06bb892afed7848b3726bbad2d6a846fd59d77d2
tot20482 <93470801+nhlinhseuit@users.noreply.github.com>
2024-05-05 22:46:13 +0700

    finish chart_page

--- lib/presentations/home/home_page.dart
+++ lib/presentations/home/home_page.dart
@@ -44,7 +44,7 @@ class _HomePageState extends State<HomePage> {
         index: _selectedIndex,
         children: tabs,
       ),
-      backgroundColor: AppColor.primaryDarkBackground,
+      backgroundColor: Colors.transparent,
       bottomNavigationBar: Container(
         decoration: const BoxDecoration(
             color: AppColor.backgroundSecondary,
