c5523ba610f05774fd931bacd24accca4d163e94
nhlinhseuit <93470801+nhlinhseuit@users.noreply.github.com>
2024-05-29 23:32:20 +0700

    news

--- lib/data/mock_data.dart
+++ lib/data/mock_data.dart
@@ -48,6 +48,55 @@ class MockData {
       img:
           'https://www.usatoday.com/money/blueprint/images/uploads/2023/10/04101024/GettyImages-629743180.jpg?width=700&fit=cover&format=webp',
     ),
+
+    //
+
+    const GoldNews(
+      title:
+          'Daily Gold News: Wednesday, June 15 – Gold Bounces From \$1,800 Ahead of the Important FOMC Release',
+      desc:
+          'The price of gold traded at \$2,363.01 per troy ounce, as of 9 a.m. ET. That’s down 2.05% from yesterday’s gold price per ounce and up 14.36% since the beginning of the yearThe gold futures contract lost 1.00% on Tuesday, June 14, as it extended its Monday’s decline of 2.3%. The market retraced last Friday’s advance following higher-than-expected Consumer Price Index release. Gold went closer to the \$1,800 level again on Monday. This morning gold is retracing its yesterday’s drop, as we can see on the daily chart (the chart includes today’s intraday data):',
+      link:
+          'https://www.usatoday.com/money/blueprint/investing/silver-price-05-28-2024/',
+      img: 'https://wallpaperaccess.com/full/104002.jpg',
+    ),
+    const GoldNews(
+      title: 'Silver price today: Silver is up 2.70% today',
+      desc:
+          'The price of silver opened at \$31.73 per ounce, as of 9 a.m. ET. That’s up 2.70% from the previous day’s silver price per ounce and up 32.60% since the beginning of the year.',
+      link:
+          'https://www.usatoday.com/money/blueprint/investing/silver-price-05-28-2024/',
+      img:
+          'https://www.usatoday.com/money/blueprint/images/uploads/2023/12/08113756/GettyImages-1456168733-1.jpg?width=700&fit=cover&format=webp',
+    ),
+    const GoldNews(
+      title: 'Gold price today: Gold is up 14.36% year to date',
+      desc:
+          'The price of gold traded at \$2,363.01 per troy ounce, as of 9 a.m. ET. That’s down 2.05% from yesterday’s gold price per ounce and up 14.36% since the beginning of the year.',
+      link:
+          'https://www.usatoday.com/money/blueprint/investing/gold-price-05-23-2024/',
+      img:
+          'https://www.usatoday.com/money/blueprint/images/uploads/2023/10/04101024/GettyImages-629743180.jpg?width=700&fit=cover&format=webp',
+    ),
+    const GoldNews(
+      title:
+          'Gold, silver and platinums red-hot rally still has further to go, strategists say',
+      desc:
+          'Precious metal prices got a major boost Wednesday after softer-than-expected U.S. inflation data heightened the near-term prospect of rate cuts from the Federal Reserve.',
+      link:
+          'https://www.msn.com/en-xl/money/markets/gold-silver-and-platinum-s-red-hot-rally-still-has-further-to-go-strategists-say/ar-BB1mv6MF',
+      img:
+          'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1mvg24.img?w=768&h=402&m=6',
+    ),
+    const GoldNews(
+      title: 'How to invest in gold',
+      desc:
+          'The gold market offers plenty of opportunities for investors.Unlike governments that print fiat currencies like the U.S. dollar or British pound, or companies that issue stocks, the world has a limited supply of physical gold. ',
+      link:
+          'https://www.usatoday.com/money/blueprint/investing/how-to-invest-in-gold/',
+      img:
+          'https://www.usatoday.com/money/blueprint/images/uploads/2023/07/01010805/how-to-invest-in-gold-e1690866522187.jpg?width=700&fit=cover&format=webp',
+    ),
   ];
   static List<DailyPrice> todaysGold = [
     DailyPrice(
